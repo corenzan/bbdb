cfc0caf4c1b1a89619c397c4e4771686b3f75a27
Arthur Corenzan <arthur@corenzan.com>
2014-08-05 23:55:57 -0300

    Add CORS

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
-require('newrelic');
-
-var express = require("express");
-var logfmt  = require("logfmt");
-var slug    = require("slug");
-var data    = require(__dirname + '/data');
-var app     = express();
+var newrelic = require('newrelic');
+var express  = require("express");
+var logfmt   = require("logfmt");
+var slug     = require("slug");
+var cors     = require("cors");
+var data     = require(__dirname + '/data');
+var app      = express();
 
 app.use(logfmt.requestLogger());
+app.use(cors());
 
 app.get('/', function(request, response) {
   if('q' in request.query) {
-    response.send(data.filter(function(doc) {
-      return slug(doc.code + ' ' + doc.name).toLowerCase().indexOf(request.query.q) > -1;
+    response.json(data.filter(function(doc) {
+      var q = request.query.q.toLowerCase();
+      var t = slug(doc.code + ' ' + doc.name).toLowerCase();
+
+      return t.indexOf(q) > -1;
     }));
   } else {
-    response.send(data);
+    response.json(data);
   }
 });
 
